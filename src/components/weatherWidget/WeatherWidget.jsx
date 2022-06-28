9cf461746bc2b1fc3b5b621e74a51db00ad459c8
Konrad Alchimowicz <alchimowiczkonrad@gmail.com>
2022-06-28 20:08:40 +0200

    Add working API call

---
+++ src/components/weatherWidget/WeatherWidget.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './weatherWidget.css';
+
+const WeatherWidget = () => {
+
+    const [weather, setWeather] = useState(0);
+
+    const getWeatherAPI = async () => {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=621fb0a9af22a4042af3054708417792');
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+    }
+
+    useEffect(() => {
+
+        getWeatherAPI();
+
+    }, []);
+
+    return (
+        <h1>test</h1>
+    )
+
+}
+
+export default WeatherWidget;
